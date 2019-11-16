0fe696980b368ccee334e6304a3f1cd51e2ae372
laums25 <56890898+laums25@users.noreply.github.com>
2019-11-16 16:01:59 -0500

    algo

---
+++ Ejercicio28.cpp
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(1)
+
+data = np.loadtxt("d2d.dat")
+
+plt.plot(data[:,0], data[:,1])
+plt.plot(data[:,0], data[:,2])
+plt.plot(data[:,0], data[:,3])
+plt.plot(data[:,0], data[:,4])
+plt.plot(data[:,0], data[:,5])
+plt.plot(data[:,0], data[:,6])
+plt.plot(data[:,0], data[:,7])
+
+plt.title("hola")
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.savefig("Ejercicio28.png")
